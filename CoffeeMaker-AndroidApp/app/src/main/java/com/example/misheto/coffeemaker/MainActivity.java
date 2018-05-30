fce3cf740ab4afabe498e3833cbdf95b53804ba2
MihaelaGadzhalova707 <mihaelagadzhalova@gmail.com>
2018-05-30 23:15:25 +0300

    it's close to working

---
+++ CoffeeMaker-AndroidApp/app/src/main/java/com/example/misheto/coffeemaker/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.misheto.coffeemaker;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView item;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnHit = (Button) findViewById(R.id.btnHit);
+        item = (TextView) findViewById(R.id.JsonItem);
+        btnHit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                HttpURLConnection connection = null;
+                try {
+                    URL url = new URL("rety.dynu.net/on_off");
+                    connection = (HttpURLConnection) url.openConnection();
+                    connection.connect();
+                    //item.setText("Button Clicked");
+                }catch (MalformedURLException e) {
+                    e.printStackTrace();
+                }catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
+
+    }
+}
